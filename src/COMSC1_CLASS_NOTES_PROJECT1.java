010167dc41d3e3d66e27923f15a4f8c5906f5c03
Dakota Davis <davisds@student.swosu.edu>
2015-10-07 09:42:29 -0500

    re-committing print line

--- src/COMSC1_CLASS_NOTES_PROJECT1.java
+++ src/COMSC1_CLASS_NOTES_PROJECT1.java
@@ -1,11 +1,9 @@
 
 public class COMSC1_CLASS_NOTES_PROJECT1 {
-
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello Class");
+		
+
 
 	}
 
