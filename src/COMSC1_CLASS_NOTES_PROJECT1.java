9a71062db4125978cddb9d07606f713d8ada70b3
Dakota Davis <davisds@student.swosu.edu>
2015-10-07 09:48:25 -0500

    Created problem outline

--- src/COMSC1_CLASS_NOTES_PROJECT1.java
+++ src/COMSC1_CLASS_NOTES_PROJECT1.java
@@ -3,6 +3,26 @@ public class COMSC1_CLASS_NOTES_PROJECT1 {
 	public static void main(String[] args) {
 		System.out.println("Hello Class");
 		
+		
+		//Outline for addition problem.
+		
+		//Round 1
+		// Generate 2 random numbers
+		// Ask the user to add these two numbers together
+		// Read in their response
+		// Check if the answer was correct
+		//  If correct, display that it was correct
+		//   Give them points
+		//   Make the next question harder
+		//  If not correct, display it was incorrect
+		//   Display the correct answer
+		//   Do not give them points
+		//   Restart round 1
+		//Round 2
+		
+		//Round 3
+		
+		//Round 4
 
 
 	}
